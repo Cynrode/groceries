c48f24f21dd7202933f93c5e4197b950e28cfc0b
Garrett Brown <Just_Garrett@protonmail.com>
2022-07-25 13:08:40 -0500

    create basic html and css

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let groceryList = [bacon, milk]
+let recipeList = [milk, egg]
+let tempList = []
+
+tempList = recipeList.concat(groceryList);
+
+let  tempSet = Set(tempList)
+for(i = 0, i < tempSet.length, i++){
+     
+}
